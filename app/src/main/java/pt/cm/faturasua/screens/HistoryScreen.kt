bf9e38d497d42228b9ff4997618da5c0c1e5ad97
João Tomás <jtsimoes@ua.pt>
2023-11-01 04:47:30 +0000

    Added info display about invoice type and invoice status Added time (hours and minutes) display on timestamp of scan/upload of invoice Improved card colors according to their respective status Fixed text overflow on card titles Improved example invoices

--- app/src/main/java/pt/cm/faturasua/screens/HistoryScreen.kt
+++ app/src/main/java/pt/cm/faturasua/screens/HistoryScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -24,10 +25,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @Preview
@@ -48,27 +51,36 @@ fun HistoryScreen(){
             textAlign = TextAlign.Left
         )
 
-        InvoiceCard(id = 1, number = "FS 06030652201010619/015229", title = "COMPRA MCDONALDS", amount = 102, date = "20220214", nif = 509441130, iva = 1.66)
-        InvoiceCard(id = 2, number = "FS 06030652201010619/015229", title = "PAGAMENTO DE SERVICOS", amount = 1235.99, date = "20220221", nif = 509441130, iva = 1.66)
-        InvoiceCard(id = 3, number = "FS 06030652201010619/015229", title = "LEVANTAMENTO MULTIBANCO", amount = 51.5, date = "20220222", nif = 509441130, iva = 1.66)
-        InvoiceCard(id = 4, number = "FS 06030652201010619/015229", title = "COMPRA QUIOSQUE", amount = 99.99, date = "20220223", nif = 509441130, iva = 1.66)
-        InvoiceCard(id = 5, number = "FS 06030652201010619/015229", title = "COMPRA RAMONA", amount = 4.2, date = "20220224", nif = 509441130, iva = 1.66)
-        InvoiceCard(id = 6, number = "FS 06030652201010619/015229", title = "COMPRA GLICINIAS PLAZA", amount = 4.2, date = "20230227", nif = 509441130, iva = 1.66)
-        InvoiceCard(id = 7, number = "FS 06030652201010619/015229", title = "COMPRA TEXTO EXTREMAMENTE LARGO OVERFLOW", amount = 111.69, date = "20230720", nif = 509441130, iva = 1.66)
+        InvoiceCard(type = "FS", number = "FS 0603065224354619/2349", title = "COMPRA MCDONALDS", amount = 102, date = "20220214", nif = 509441130, iva = 1.66, status = null)
+        InvoiceCard(type = "FT", number = "FT 11435234619/015229", title = "PAGAMENTO DE SERVICOS", amount = 1235.99, date = "20220221", nif = 509441130, iva = 1.66, status = true)
+        InvoiceCard(type = "ND", number = "ND 23406234321010619/222", title = "LEVANTAMENTO MULTIBANCO", amount = 51.5, date = "20220222", nif = 509441130, iva = 1.66, status = true)
+        InvoiceCard(type = "NC", number = "NC 123349/015229", title = "COMPRA QUIOSQUE", amount = 99.99, date = "20220223", nif = 509441130, iva = 1.66, status = true)
+        InvoiceCard(type = "RC", number = "RC 732340010619/123", title = "COMPRA RAMONA", amount = 4.2, date = "20220224", nif = 509441130, iva = 1.66, status = true)
+        InvoiceCard(type = "OR", number = "OR 123652201010619/019", title = "COMPRA GLICINIAS PLAZA", amount = 4.2, date = "20230227", nif = 509441130, iva = 1.66, status = false)
+        InvoiceCard(type = "FS", number = "FS 2142/210", title = "COMPRA TEXTO EXTREMAMENTE LARGO OVERFLOW", amount = 111.69, date = "20230720", nif = 509441130, iva = 1.66, status = true)
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun InvoiceCard(id: Int, number: String, title: String, amount: Number, date: String, nif: Number, iva: Number) {
+fun InvoiceCard(type: String, number: String, title: String, amount: Number, date: String, nif: Number, iva: Number, status : Boolean?) {
 
     var expanded by remember { mutableStateOf (false) }
+
+    val color = when (status) {
+        true -> MaterialTheme.colorScheme.surfaceTint
+        false -> MaterialTheme.colorScheme.errorContainer
+        else -> MaterialTheme.colorScheme.tertiaryContainer
+    }
 
     Card(
         onClick = { expanded = !expanded },
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 8.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = color
         ),
         modifier = Modifier
             .fillMaxWidth()
@@ -86,18 +98,21 @@ fun InvoiceCard(id: Int, number: String, title: String, amount: Number, date: St
             ) {
                 Column {
                     Text(
-                        text = LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE).format(
-                            DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                        text = LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE).format(DateTimeFormatter.ofPattern("dd/MM/yyyy")),
                         style = MaterialTheme.typography.labelSmall,
                         modifier = Modifier.padding(horizontal = 5.dp),
-                        color = MaterialTheme.colorScheme.tertiary,
                         textAlign = TextAlign.Left,
                     )
                     Text(
                         text = title,
                         style = MaterialTheme.typography.titleMedium,
-                        modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp),
+                        modifier = Modifier
+                            .padding(horizontal = 5.dp, vertical = 2.dp)
+                            .width(230.dp),
                         textAlign = TextAlign.Left,
+                        color = MaterialTheme.colorScheme.tertiary,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1
                     )
                 }
                 Text(
@@ -111,41 +126,68 @@ fun InvoiceCard(id: Int, number: String, title: String, amount: Number, date: St
 
             if (expanded) {
                 Text(
-                    text = "Identifier: #$id",
+                    text = "Status: ${if (status == true) "Approved ✅" else if (status == false) "Rejected ❌" else "Pending 🕑"}",
                     style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.tertiary,
                     modifier = Modifier.padding(start = 5.dp, end = 5.dp, top = 10.dp, bottom = 2.dp)
+                )
+                // TODO: Hardcoded type (for now)
+                val type = "FS"
+                var formatType : String = type
+                when (type) {
+                    "OR" -> formatType = "Orçamento"
+                    "FP" -> formatType = "Fatura Pró-forma"
+                    "FT" -> formatType = "Fatura"
+                    "FS" -> formatType = "Fatura Simplificada"
+                    "FR" -> formatType = "Fatura Recibo"
+                    "ND" -> formatType = "Nota de Débito"
+                    "NC" -> formatType = "Nota de Crédito"
+                    "FCA" -> formatType = "Fatura de Auto-faturação"
+                    "RC" -> formatType = "Recibo"
+                }
+                Text(
+                    text = "Invoice type: $formatType",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp)
                 )
                 Text(
                     text = "Invoice number: $number",
                     style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.tertiary,
                     modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp)
                 )
                 Text(
                     text = "NIF: ${nif.toString().chunked(3).joinToString(separator = " ")}",
                     style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.tertiary,
                     modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp)
                 )
                 var value : Number = (amount.toDouble() -  iva.toDouble())
                 Text(
                     text = "Valor líquido (sem IVA): ${formatPrice(value)}€",
                     style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.tertiary,
                     modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp)
                 )
                 Text(
                     text = "Valor IVA: ${formatPrice(iva)}€",
                     style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.tertiary,
                     modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp)
                 )
                 Text(
                     text = "Valor total pago: ${formatPrice(amount)}€",
                     style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.tertiary,
                     modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp)
                 )
-                // TODO: Hardcoded timestamp
-                val formatTimestamp = LocalDate.parse("2023-10-31T19:06:14.963943308", DateTimeFormatter.ISO_DATE_TIME).format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) // TODO: Added time (HH:mm:ss), careful with timezones
+                // TODO: Hardcoded timestamp (for now)
+                val formatTimestamp = LocalDateTime.parse("2023-10-31T19:06:14.963943308", DateTimeFormatter.ISO_DATE_TIME).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
                 Text(
                     text = "Digitalizada em: $formatTimestamp",
                     style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.tertiary,
                     modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp)
                 )
             }
