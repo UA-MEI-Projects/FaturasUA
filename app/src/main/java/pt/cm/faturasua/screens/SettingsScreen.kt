56359fd5f336fb5aaea86981a5ebeef9d1c31737
João Tomás <jtsimoes@ua.pt>
2023-11-04 21:48:24 +0000

    Improved language selector on settings page Fixed dark mode colors on dropdown menu

--- app/src/main/java/pt/cm/faturasua/screens/SettingsScreen.kt
+++ app/src/main/java/pt/cm/faturasua/screens/SettingsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -25,13 +26,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.os.LocaleListCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.compose.FaturasUATheme
+import pt.cm.faturasua.R
 import pt.cm.faturasua.utils.PreferencesManager
 import pt.cm.faturasua.viewmodel.UserViewModel
 import java.util.Locale
@@ -43,6 +47,9 @@ fun SettingsScreen(
     userViewModel: UserViewModel = viewModel()
 ){
     val preferencesManager = remember{ PreferencesManager(context)}
+    var languageOn by remember {
+        mutableStateOf(preferencesManager.getData(PreferencesManager.PREFERENCE_CODE_LANGUAGE, "English"))
+    }
     var darkModeOn by remember {
         mutableStateOf(preferencesManager.getData(PreferencesManager.PREFERENCE_CODE_DARK_MODE, false))
     }
@@ -55,30 +62,36 @@ fun SettingsScreen(
             .fillMaxSize()
             .padding(20.dp)
     ) {
-
-        val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags("xx-YY")
-        Text(text = "$appLocale")
-
-        val currentLang: String = Locale.getDefault().getDisplayName()
-        Text(text = "$currentLang")
+        val deviceLanguage: String = Locale.getDefault().displayLanguage.replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(
+                Locale.getDefault()
+            ) else it.toString()
+        }
+        val optionsList : List<String> = listOf(
+            stringResource(
+                R.string.settings_device_language_option,
+                deviceLanguage
+            ), "English", "Português")
 
         LanguageDropdownMenu(
-            options = listOf("English", "Portuguese"),
-            selected = "English",
-            label = "Language",
+            options = optionsList,
+            selected = languageOn,
+            label = stringResource(R.string.settings_language_label),
             onOptionChange = { selectedOption ->
                 println("LanguageDropdownMenu - Selected option: $selectedOption")
                 val locale : String = when (selectedOption) {
-                    "Portuguese", "Português" -> "pt" // or pt_PT ?
+                    optionsList[0] -> "auto"
+                    optionsList[2] -> "pt" // or pt_PT ?
                     else -> "en" // or en_US ?
                 }
+                // TODO: Force call language 'locale' according to the user choice instead of using device auto
                 println("LanguageDropdownMenu - Locale file: $locale")
-                // TODO: Create and call language 'locale' according to the user choice
+                preferencesManager.saveData(PreferencesManager.PREFERENCE_CODE_LANGUAGE, selectedOption)
             }
         )
         Spacer(modifier = Modifier.size(20.dp))
         ToggleButton(
-            label = "Dark mode",
+            label = stringResource(R.string.settings_dark_mode),
             value = darkModeOn,
             onCheckedChange = {
                 darkModeOn = !darkModeOn
@@ -88,7 +101,7 @@ fun SettingsScreen(
         )
         Spacer(modifier = Modifier.size(20.dp))
         ToggleButton(
-            label = "Notifications",
+            label = stringResource(R.string.settings_notifications),
             value = notifOn,
             onCheckedChange = {
                 notifOn = !notifOn
@@ -158,7 +171,12 @@ fun LanguageDropdownMenu(
                 onValueChange = {},
                 label = { Text(label) },
                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                colors = ExposedDropdownMenuDefaults.textFieldColors(
+                    unfocusedTextColor = Color.Black,
+                    focusedTextColor = Color.Black,
+                    unfocusedLabelColor = Color.DarkGray,
+                    focusedLabelColor = Color.DarkGray,
+                ),
             )
             ExposedDropdownMenu(
                 expanded = expanded,
