47e48cc0a89a57ae732b947bf4537f5aa2943489
João Tomás <jtsimoes@ua.pt>
2023-11-06 20:18:38 +0000

    Added error handling on auth screen Improved login and sign up screens overall

--- app/src/main/java/pt/cm/faturasua/screens/AuthScreen.kt
+++ app/src/main/java/pt/cm/faturasua/screens/AuthScreen.kt
@@ -1,5 +1,6 @@
 package pt.cm.faturasua.screens
 
+import android.view.Gravity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -11,9 +12,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -23,13 +33,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogWindowProvider
 import pt.cm.faturasua.R
 import pt.cm.faturasua.utils.FirebaseUtil
 
@@ -39,9 +56,37 @@ fun AuthScreen(
     firebaseUtil: FirebaseUtil,
     onChangeAdminMode: () -> Unit
 ) {
-    var unlockAdminCounter by remember {
-        mutableIntStateOf(0)
+    val openAlertDialogErrorEmptyFields = remember { mutableStateOf(false) }
+    val openAlertDialogErrorInvalidFields = remember { mutableStateOf(false) }
+    val openAlertDialogErrorWrongCredentials = remember { mutableStateOf(false) }
+
+    when {
+        openAlertDialogErrorEmptyFields.value -> {
+            AlertDialogError(
+                onDismissRequest = { openAlertDialogErrorEmptyFields.value = false },
+                dialogTitle = stringResource(R.string.auth_error_dialog_title) + "!",
+                dialogText = stringResource(R.string.auth_error_dialog_empty_fields),
+                icon = Icons.Default.Warning
+            )
+        }
+        openAlertDialogErrorInvalidFields.value -> {
+            AlertDialogError(
+                onDismissRequest = { openAlertDialogErrorInvalidFields.value = false },
+                dialogTitle = stringResource(R.string.auth_error_dialog_title) + "!",
+                dialogText = stringResource(R.string.auth_error_dialog_invalid_fields),
+                icon = Icons.Default.Warning
+            )
+        }
+        openAlertDialogErrorWrongCredentials.value -> {
+            AlertDialogError(
+                onDismissRequest = { openAlertDialogErrorWrongCredentials.value = false },
+                dialogTitle = stringResource(R.string.auth_error_dialog_title) + "!",
+                dialogText = stringResource(R.string.auth_error_dialog_wrong_credentials),
+                icon = Icons.Default.Warning
+            )
+        }
     }
+
     var adminSignIn by remember{
         mutableStateOf(false)
     }
@@ -62,87 +107,130 @@ fun AuthScreen(
         verticalArrangement = Arrangement.SpaceEvenly
 
     ) {
-        Button(
-            onClick = {
-                if(!adminSignIn){
-                    unlockAdminCounter += 1
+        Image(
+            painter = painterResource(id = R.drawable.app_logo),
+            contentDescription = "Logo",
+            modifier = Modifier.size(150.dp)
+        )
 
-                    if(unlockAdminCounter > 5){
-                        adminSignIn = !adminSignIn
-                    }
-                }
-                else{
-                    adminSignIn = !adminSignIn
-                }
-            }) {
-            Image(painter = painterResource(id = R.drawable.app_logo),
-                contentDescription = "Logo",
-                modifier = Modifier.size(150.dp)
-            )
-        }
-
-        // SIGN IN PARA ADMIN
+        // ADMIN LOGIN
         if(adminSignIn){
             AdminMenu(onClick = { token, password ->
-                if(token.equals("") && password.equals(""))
+                if(token == "" || password == "")
+                    openAlertDialogErrorEmptyFields.value = true
+                else if(token != "admin" && password != "admin")    // Hardcoded admin credentials
+                    openAlertDialogErrorWrongCredentials.value = true
+                else
                     onChangeAdminMode()
             })
+
+            FilledTonalButton(
+                content = { Text("Go back") },
+                onClick = { adminSignIn = !adminSignIn }
+            )
         }
-        // SIGN IN NORMAL
+        // USER LOGIN
         else{
             if(alreadySignedIn){
                 if(expanded){
                     ExpandedMenu(
-                        buttonText = "Sign In",
+                        buttonText = "Log in",
                         onClick = { email, password, username, phoneNumber ->
-                            if(email != "" && password != "")
-                                firebaseUtil.signIn(email = email, password = password)
+                            if(email == "" || password == "")
+                                openAlertDialogErrorEmptyFields.value = true
+                            else if (!email.contains("@"))
+                                openAlertDialogErrorInvalidFields.value = true
+                            else {
+                                if (!firebaseUtil.signIn(email = email, password = password))
+                                    openAlertDialogErrorWrongCredentials.value = true
+                            }
                         },
                         signUp = false
                     )
-                }
-                else{
-                    Button(onClick = { expanded = !expanded }) {
-                        Text("Sign in")
-                    }
-                }
-                ClickableText(
-                    text = AnnotatedString("Not signed in?"),
-                    onClick = {
-                        alreadySignedIn = !alreadySignedIn
-                    }
-                )
-            }
-            else{
-                if(expanded){
-                    ExpandedMenu(
-                        buttonText = "Sign Up",
-                        onClick = { email, password, username, phoneNumber ->
-                            if(email != "" && password != "")
-                                firebaseUtil.createAccount(
-                                    email = email,
-                                    password = password,
-                                    displayName = username,
-                                    phoneNumber = phoneNumber
-                                )
-                        },
-                        signUp = true
+
+                    FilledTonalButton(
+                        content = { Text("Go back") },
+                        onClick = { expanded = !expanded }
                     )
                 }
                 else{
                     Button(onClick = { expanded = !expanded }) {
-                        Text("Sign up")
+                        Text(stringResource(R.string.auth_user_login_button))
+                    }
+
+                    Button(
+                        onClick = { adminSignIn = !adminSignIn }
+                    ){
+                        Text(stringResource(R.string.auth_admin_login_button))
+                    }
+
+                    Column (
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = "New to FaturasUA?", style = MaterialTheme.typography.bodyMedium)
+                        FilledTonalButton(
+                            content = { Text("Create an account first") },
+                            modifier = Modifier.padding(5.dp),
+                            onClick = {
+                                alreadySignedIn = !alreadySignedIn
+                            }
+                        )
                     }
                 }
-                ClickableText(
-                    text = AnnotatedString("Already signed in?"),
-                    onClick = {
-                        alreadySignedIn = !alreadySignedIn
-                    }
+            }
+
+            // USER SIGN UP
+            else{
+                ExpandedMenu(
+                    buttonText = "Create account",
+                    onClick = { email, password, username, phoneNumber ->
+                        if(email == "" || password == "" || username == "" || phoneNumber == "")
+                            openAlertDialogErrorEmptyFields.value = true
+                        else if (!email.contains("@") || password.length < 8)
+                            openAlertDialogErrorInvalidFields.value = true
+                        else
+                            firebaseUtil.createAccount(
+                                email = email,
+                                password = password,
+                                displayName = username,
+                                phoneNumber = phoneNumber
+                            )
+                    },
+                    signUp = true
                 )
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(top = 25.dp)
+                ) {
+                    Text(text = "Already have an account?", style = MaterialTheme.typography.bodyMedium)
+                    FilledTonalButton(
+                        content = { Text("Log in instead") },
+                        modifier = Modifier.padding(5.dp),
+                        onClick = {
+                            alreadySignedIn = !alreadySignedIn
+                        }
+                    )
+                }
             }
         }
     }
+}
+
+@Composable
+fun AlertDialogError(
+    onDismissRequest: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        containerColor = MaterialTheme.colorScheme.errorContainer,
+        icon = { Icon(icon, contentDescription = stringResource(R.string.auth_error_dialog_title), tint = MaterialTheme.colorScheme.error) },
+        title = { Text(text = dialogTitle) },
+        text = { Text(text = dialogText) },
+        onDismissRequest = { onDismissRequest() },
+        confirmButton = { TextButton(onClick = { onDismissRequest() }) { Text("OK") } }
+    )
 }
 
 @Composable
@@ -154,18 +242,23 @@ fun AdminMenu(
 
     OutlinedTextField(
         value = token,
-        label = { Text(text = "Token Id") },
+        label = { Text(text = "Token") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
         onValueChange = { token = it }
     )
     Spacer(modifier = Modifier.size(10.dp))
     OutlinedTextField(
         value = password,
         label = { Text(text = "Password") },
-        onValueChange = { password = it }
+        onValueChange = { password = it },
+        visualTransformation = PasswordVisualTransformation(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Password)
     )
     Spacer(modifier = Modifier.size(10.dp))
     Button(onClick = { onClick(token, password)}) {
-        Text("")
+        Text("Enter admin panel")
     }
 }
 
@@ -182,36 +275,42 @@ fun ExpandedMenu(
     if(signUp){
         OutlinedTextField(
             value = username,
-            label = { Text(text = "Username") },
+            label = { Text(text = "Name (first and last)") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Text),
             onValueChange = { username = it }
         )
         Spacer(modifier = Modifier.size(10.dp))
         OutlinedTextField(
             value = phoneNumber,
-            label = { Text(text = "Phone Number") },
-            onValueChange = { phoneNumber = it }
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Phone),
+            label = { Text(text = "Phone number") },
+            onValueChange = { value ->
+                if (value.length <= 9) {
+                    phoneNumber = value.filter { it.isDigit() }
+                }
+            }
         )
         Spacer(modifier = Modifier.size(10.dp))
     }
     OutlinedTextField(
         value = email,
         label = { Text(text = "Email") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Email),
         onValueChange = { email = it }
     )
     Spacer(modifier = Modifier.size(10.dp))
     OutlinedTextField(
         value = password,
-        label = { Text(text = "Password") },
+        label = { Text(text = "Password (min. 8 chars)") },
         onValueChange = { password = it },
         visualTransformation = PasswordVisualTransformation(),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Password)
 
     )
-    Text(
-        text = "Mínimo 6 caracteres.",
-        color = Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 8.sp)
     Spacer(modifier = Modifier.size(50.dp))
     Button(onClick = { onClick(email, password, username, phoneNumber)}) {
         Text(buttonText)
