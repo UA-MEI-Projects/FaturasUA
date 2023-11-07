f62ff00fe832f3f56b8841aa1e62a520592b80dc
mateusalmeida <mbcsalmeida@ua.pt>
2023-11-07 16:07:04 +0000

    User Repository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,11 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.room:room-common:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     val material3_version = "1.1.2"
+    val room_version = "2.3.0"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
@@ -84,7 +87,7 @@ dependencies {
     implementation("com.google.mlkit:barcode-scanning:17.2.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
 
-
+    implementation("androidx.room:room-ktx:$room_version")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
